75a0c4769535ac0829e49dc4c1615e2f324b601b
MA LEI <apple@APPLEdeMacBook-Pro-46.local>
2015-11-09 14:26:14 +0800

    Initial Commit

---
+++ 0818_查看大图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0818_查看大图
+//
+//  Created by APPLE on 15/11/9.
+//  Copyright (c) 2015年 big nerd ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0818_查看大图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0818_查看大图
+//
+//  Created by APPLE on 15/11/9.
+//  Copyright (c) 2015年 big nerd ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
